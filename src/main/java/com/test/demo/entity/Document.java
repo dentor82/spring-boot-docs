4f6dfeda1b563d8724a2cfa35bf4476a87bad1d8
dtoropov <bvg0hn@ynt>
2021-03-24 21:51:14 +0400

    Объект хранения документа

---
+++ src/main/java/com/test/demo/entity/Document.java
@@ -0,0 +1,25 @@
+package com.test.demo.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "documents")
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private LocalDate date;
+
+    @ManyToOne
+    private User user;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Lob
+    private String description;
+}
